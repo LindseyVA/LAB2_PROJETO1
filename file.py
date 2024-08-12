c17aa5b0f3a76733a156ae8202cc10e2feed989e
Lindsey de Vargas de Azevedo <B43912@fgv.edu.br>
2024-08-12 18:07:45 -0300

    cria função media_sorteio

--- file.py
+++ file.py
@@ -2,3 +2,15 @@ def cumprimento(texto):
     return (f"Olá, {texto}")
 
 print(cumprimento("Lindsey de Vargas de Azevedo"))
+
+
+import random
+
+def media_sorteio():
+    # atribui um valor inteiro entre 1 e 100 às variáveis n1, n2, n3 (sorteio)
+    n1 = random.randint(0,100)
+    n2 = random.randint(0,100)
+    n3 = random.randint(0,100)
+
+    media = sum([n1, n2, n3]) / 3 # três números aleatórios
+    return media
